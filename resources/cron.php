8ba0b650dfd2e51570e2dd0a2ee518f47fdfeb0a
hariesnurikhwan <hariesnurikhwan@gmail.com>
2014-12-11 23:53:19 +0800

    Optimize fetch method to reduce time taken.
    — Add html_dom.php
    — Replace 3rd party data fetch with open source Simple HTML DOM Parser.
    — Remove unnecessary echo.
    — Reduce cronjob time taken.

--- resources/cron.php
+++ resources/cron.php
@@ -1,4 +1,9 @@
 <?php
+
+include_once('./html_dom.php');
+
+include_once('./func.php');
+
 //Server Details
 $dbUsername = "root";
 $dbPassword = "toor";
@@ -11,24 +16,16 @@ $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 if ($conn->connect_error == 1) {
     die("Connection failed: " . $conn->connect_error);
 }else{
-	$data = file_get_contents("http://api.import.io/store/data/417566c3-376f-4ba0-a614-c12b47a393db/_query?input/webpage/url=http%3A%2F%2Fwww.mks.ch%2Fmobile%2Fmyr%2Fv1%2F&_user=c578e8b1-4a72-4b53-ba11-a99d05580189&_apikey=QPOvhNrJHwaxmcFmvn7czes9mxY%2FhYOeD9QoNgWc%2FkyaWAndTJ7RLi0xaUl9NfGwL%2FiV%2Bs0OGDZtd3Vawz1SAw%3D%3D");
+	$html = file_get_html('http://www.mks.ch/mobile/myr/v1/');
 
-	$data = json_decode($data);
+	$we_buy =  removeSpace($html->find('tr[class="L10"]', 1)->find('td[align="right"]', 0)->plaintext);
 
-	$we_buy = $data->results[0]->we_buy;
-	$we_buy = number_format((float)round(preg_replace("/\s/", "", $we_buy)/1000, 2), 2, '.', '');
-	echo "We Buy : RM" . $we_buy . "/g";
+	$we_sell =  removeSpace($html->find('tr[class="L10"]', 1)->find('td[align="right"]', 1)->plaintext);
 
-	$we_sell = $data->results[0]->we_sell;
-	$we_sell = number_format((float)round(preg_replace("/\s/", "", $we_sell)/1000, 2), 2, '.', '');
-	echo " We Sell : RM" . $we_sell . "/g";
-
-	$d_time = $data->results[0]->time;
-	echo " Data Time : " . $d_time;
+	$d_time =  formatDatetime($html->find('tr[class="L0"]', 0)->find('font.date', 0)->plaintext);
 
 	date_default_timezone_set("Asia/Kuala_Lumpur");
 	$s_time = date("Y-m-d H:i:s");
-	echo " Server Time : " . $s_time;
 
 	if ($we_buy != "0.00" && $we_sell != "0.00") {
 		$sql = "INSERT INTO `mks_price` (`we_buy`, `we_sell`, `data_time`, `server_time`) VALUES ('$we_buy', '$we_sell', '$d_time', '$s_time')";
