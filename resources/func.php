bc560ea9e5c8dc2d498a77b78f9ecc8421630697
hariesnurikhwan <hariesnurikhwan@gmail.com>
2014-12-11 23:44:12 +0800

    Add function removeSpace & function formatDatetime

--- resources/func.php
+++ resources/func.php
@@ -71,4 +71,12 @@
 			}
 		}
 	}
+
+	function removeSpace($data){
+	return number_format((float)round(preg_replace("/\s/", "", $data)/1000, 2), 2, '.', '');
+	}
+
+	function formatDatetime($data){
+		return preg_replace("/CET/", "", $data);
+	}
 ?>
