d77e8c5833358d6356979611a22a313e2aa7d15a
Haries Nur Ikhwan <hariesnurikhwan@users.noreply.github.com>
2014-12-21 13:22:44 +0800

    Improve versioning footer.

--- resources/git.php
+++ resources/git.php
@@ -2,16 +2,16 @@
 class QuickGit {
   public $version;
   
-  function __construct() {
+  function __construct($repo) {
     exec('git describe --always',$version_mini_hash);
     exec('git rev-list HEAD | wc -l',$version_number);
     exec('git log -1',$line);
-    $this->version['short'] = "mksindicativeprice - v1.".trim($version_number[0])." (".$version_mini_hash[0].")";
+    $this->version['short'] = "$repo - v".trim($version_number[0]/100)." (".$version_mini_hash[0].")";
   }
 
   public function output($type) {
     return $this->version[$type];
   }
 }
-$git = new QuickGit;
+$git = new QuickGit('mksindicativeprice');
 ?>
